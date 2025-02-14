904ec41e213ff54082a1084e67537a8b453f915f
17Shanks <17akashanks@gmail.com>
2025-02-14 16:25:06 +0530

    Update presentationGenerator.js

--- backend/presentationGenerator.js
+++ backend/presentationGenerator.js
@@ -7,7 +7,7 @@ const { generatePresentation } = require("./presentationGenerator");
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.use(bodyParser.json());
 
